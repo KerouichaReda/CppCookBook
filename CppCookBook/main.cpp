512d14ab9d756d346fd9fe263f8f526ab64ed70b
KerouichaReda <kerouicha.reda@gmail.com>
2019-01-08 17:06:28 +0800

    6.3 Copying a vector

--- CppCookBook/main.cpp
+++ CppCookBook/main.cpp
@@ -1,13 +1,31 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<algorithm>
 #include"vectorTkit.h"
 
 int main(void)
 {
-	std::string defString = "initialization";
-	std::vector<std::string> vec(50, defString);
-	vecPrint(vec);
+	std::vector<std::string> vec(5);
+	std::string fool[] = { "My","way","or","the","highway" };
+		vec[0] = "Today";
+		vec[1] = "is ";
+		vec[2] = "a";
+		vec[3] = "new";
+		vec[4] = "day";
+
+		std::vector<std::string> vec2(vec);
+		vecPrint(vec2);
+
+		vec.at(0) = "Tommorow";
+
+		vec2.assign(vec.begin(), vec.end());
+		vecPrint(vec2);
+
+		std::vector<std::string>::iterator p;
+		p = std::find(vec.begin(), vec.end(), "end");
+		vec2.assign(vec.begin(), p);
+
 	getchar();
 	return 0;
 }
