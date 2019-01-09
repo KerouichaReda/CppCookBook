5db01413ebb5ff631ca7bc89616b3a2dc09a1888
KerouichaReda <kerouicha.reda@gmail.com>
2019-01-09 15:20:30 +0800

    6.6 Mapping strings to Other Things

--- CppCookBook/main.cpp
+++ CppCookBook/main.cpp
@@ -1,58 +1,33 @@
 #include<iostream>
-#include<list>
+#include<map>
 #include<string>
-#include<algorithm>
-#include <functional>
 #include"vectorTkit.h"
 
-//printing function
-template<typename T>
-struct printer
-{
-	void operator()(const T& s)
-	{
-		std::cout << s << std::endl;
-	}
-};
-bool inline even(int n)
-{
-	return(n % 2 == 0);
-}
-printer<std::string> strPrinter;
-printer<int> intPrinter;
 
 int main(void)
 {
-	std::list < std::string > lstOne;
-	std::list < std::string > lstTwo;
-	lstOne.push_back("Green");
-	lstOne.push_back("Red");
-	lstOne.push_back("Bleu");
+	std::map < std::string, std::string > strMap;
+	strMap["Monday"] = "Montag";
+	strMap["Tuesday"] = "Dienstag";
+	strMap["Wednesday"] = "Mittwoch";
+	strMap["Thursday"] = "Donnerstag";
+	strMap["Friday"] = "Freitag";
+	strMap["Saturday"] = "Samstag";
+	strMap.insert(std::pair< std::string, std::string>("Sunday", "Sonntag"));
 
-	lstTwo.push_front("Cian");
-	lstTwo.push_front("Magenta");
-	lstTwo.push_front("Yellow");
-
-	std::for_each(lstOne.begin(), lstOne.end(), strPrinter);
-	
-	lstOne.sort();
-	lstTwo.sort();
-
-	lstOne.merge(lstTwo);
-	std::for_each(lstOne.begin(), lstOne.end(), strPrinter);
-
-	std::list<int> intLst;
-	for (int i = 0; i < 50; i++)
+	for (std::map<std::string,std::string>::iterator p = strMap.begin(); p !=strMap.end(); p++)
 	{
-		intLst.push_back(i);
+		std::cout << "English: " << p->first << " German: " << p->second << std::endl;
 	}
+	std::cout << std::endl;
 
-	std::for_each(intLst.begin(), intLst.end(), intPrinter);
+	//strMap.erase("Thursday");
+	strMap.erase(strMap.find("Thursday"));
 
-	intLst.remove_if(std::bind2nd(std::greater<int>(), 25));
-	std::for_each(intLst.begin(), intLst.end(), intPrinter);
-	intLst.remove_if(even);
-	std::for_each(intLst.begin(), intLst.end(), intPrinter);
+	for (std::map<std::string, std::string>::iterator p = strMap.begin(); p != strMap.end(); p++)
+	{
+		std::cout << "English: " << p->first << " German: " << p->second << std::endl;
+	}
 	getchar();
 	return 0;
 }
---
+++ q
@@ -0,0 +1,37 @@
+[33mcommit af302655d15a23553135014e56124212175d4c95[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31mCppCookBook/master[m[33m)[m
+Author: KerouichaReda <kerouicha.reda@gmail.com>
+Date:   Wed Jan 9 11:56:10 2019 +0800
+
+    6.5 Storing Objects in a list
+
+[33mcommit 3665256b8afc3f999d4fc4c13614d895a5e63cdf[m
+Author: KerouichaReda <kerouicha.reda@gmail.com>
+Date:   Tue Jan 8 18:16:25 2019 +0800
+
+    6.4 Storing Pointers in a vector
+
+[33mcommit 512d14ab9d756d346fd9fe263f8f526ab64ed70b[m
+Author: KerouichaReda <kerouicha.reda@gmail.com>
+Date:   Tue Jan 8 17:06:28 2019 +0800
+
+    6.3 Copying a vector
+
+[33mcommit ea4525b09ad99f54495772174db9bbc3fcc09f39[m
+Author: KerouichaReda <kerouicha.reda@gmail.com>
+Date:   Tue Jan 8 16:23:54 2019 +0800
+
+    6.2 Using vectors Efficiently
+
+[33mcommit adc6033e41f2abb1cb07bab226d02354220ec044[m
+Author: KerouichaReda <kerouicha.reda@gmail.com>
+Date:   Wed Jan 2 16:40:10 2019 +0800
+
+    6.1 Using vectors Instead of Arrays
+    
+    c++ cookbook page 214
+
+[33mcommit 481bd8b980c26e05340bc24d947258637631fd78[m
+Author: KerouichaReda <kerouicha.reda@gmail.com>
+Date:   Wed Jan 2 16:05:12 2019 +0800
+
+    Initial commit
